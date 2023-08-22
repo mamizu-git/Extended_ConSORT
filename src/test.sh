6c9cd0215f253eccd7bced837577cc27cee18a38
root <tanaka-izumi@g.ecc.u-tokyo.ac.jp>
2023-08-22 10:25:13 +0900

    add result

--- src/test.sh
+++ src/test.sh
@@ -4,3 +4,4 @@ z3 ../experiment/out_int.smt2 > ../experiment/result_int
 z3 ../experiment/out_fv.smt2 > ../experiment/result
 ./main chc $1
 hoice ../experiment/out_chc.smt2 > ../experiment/chc_result
+z3 ../experiment/out_chc.smt2 > ../experiment/chc_result_sub
