06d6beb0f10db8126cd4130f309766ff11533455
Ken Sakayori <kensakayori+research@gmail.com>
2023-09-14 23:14:02 +0900

    use variables to invoke Z3 and HoICE

--- test.sh
+++ test.sh
@@ -1,11 +1,13 @@
 #! /bin/bash
+Z3=z3
+HOICE=hoice
 j=3
 while true
 do
 	./src/main int $1 $j
-	z3 experiment/out_int.smt2 > experiment/result_int
+	$Z3 experiment/out_int.smt2 > experiment/result_int
 	./src/main fv $1 0
-	z3 experiment/out_fv.smt2 > experiment/result
+	$Z3 experiment/out_fv.smt2 > experiment/result
 	st=`head -n 1 experiment/result`
 	if [ $st = "sat" ] ; then
 		break
@@ -15,7 +17,7 @@ done
 echo "range: $j"
 echo "ownership: $st"
 ./src/main chc $1 0
-hoice experiment/out_chc.smt2 > experiment/chc_result
+$HOICE experiment/out_chc.smt2 > experiment/chc_result
 st2=`head -n 1 experiment/chc_result` 
 echo "refinement: $st2"
 echo ""
