bc43ae50f70507687eb5f179df35d68149d2d5c6
root <tanaka-izumi@g.ecc.u-tokyo.ac.jp>
2023-09-15 15:49:54 +0900

    update sh

--- test.sh
+++ test.sh
@@ -1,20 +1,25 @@
+#! /bin/bash
+Z3=z3
+HOICE=hoice
+i=1
 j=3
 while true
 do
+	i=$(($i+1))
 	./src/main int $1 $j
-	z3 experiment/out_int.smt2 > experiment/result_int
+	$Z3 experiment/out_int.smt2 > experiment/result_int
 	./src/main fv $1 0
-	z3 experiment/out_fv.smt2 > experiment/result
+	$Z3 experiment/out_fv.smt2 > experiment/result
 	st=`head -n 1 experiment/result`
 	if [ $st = "sat" ] ; then
 		break
 	fi
 	j=$(($j+1))
 done
-echo "range: $j"
+echo "iter: $i"
 echo "ownership: $st"
 ./src/main chc $1 0
-hoice experiment/out_chc.smt2 > experiment/chc_result
+$HOICE experiment/out_chc.smt2 > experiment/chc_result
 st2=`head -n 1 experiment/chc_result` 
 echo "refinement: $st2"
 echo ""
--- test_all.sh
+++ test_all.sh
@@ -1,8 +1,39 @@
-for file in examples/bench/*; do
-    echo "$file:"
-    time -p timeout 180 ./test.sh $file
-    if [ $? -ne 0 ]; then
-        echo "Timeout"
-    fi
-    echo ""
-done
+#! /bin/bash
+
+function verbose (){
+    for file in examples/bench/*; do
+        echo "$file:"
+        time -p timeout 180 ./test.sh $file
+        if [ $? -ne 0 ]; then
+            echo "Timeout"
+        fi
+        echo ""
+    done
+}
+
+
+function gen_table(){
+    # Only shows the "real time"
+    TIMEFORMAT='%3R'
+    for file in examples/bench/*; do
+        # Ignores the output of test.sh
+        ret=$( { time timeout 180 ./test.sh $file >/dev/null ; } 2>&1 )
+        if [ $? -ne 0 ]; then
+            ret="Timeout"
+        fi
+        echo -e "$(basename -s .imp $file)\t${ret}"
+    done
+}
+
+case $1 in
+    -t)
+      gen_table
+      ;;
+    -*)
+      echo "invalid option"
+      exit 1
+      ;;
+    *)
+      verbose
+      ;;
+esac
